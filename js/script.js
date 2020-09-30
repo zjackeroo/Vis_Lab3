318489ddd2fc42d37953b400b9f16d97dfba8c8a
Zjackeroo <jeffery_guo@hotmail.com>
2020-09-30 17:01:52 -0400

    bar chart

--- js/script.js
+++ js/script.js
@@ -10,12 +10,10 @@
 //     console.log('cities', data)
 // })
 
-data = d3.csv('./data/cities.csv', d3.autoType).then(data=>{
+d3.csv('./data/cities.csv', d3.autoType).then(data=>{
     EUdata = data.filter(d => d.eu === true);
     cityCount = 0
-    EUdata.forEach(function(d) {
-        cityCount++;
-    });
+    EUdata.forEach(d=>(cityCount++));
     console.log('EU cities', EUdata)
     d3.select('.city-count').text('Number of cities in EU: ' + cityCount)
 
@@ -29,8 +27,8 @@ data = d3.csv('./data/cities.csv', d3.autoType).then(data=>{
         .attr('cx', d=>d.x)
         .attr('cy', d=>d.y)
         .attr('r', d=>d.population>1000000?8:4)
-        .attr('fill', 'red')
-    svg.selectAll('text')
+        .attr('fill', 'salmon')
+    svg.selectAll('text.title')
         .data(EUdata)
         .enter()
         .append('text')
@@ -42,5 +40,48 @@ data = d3.csv('./data/cities.csv', d3.autoType).then(data=>{
             return (d.city + ', ' + d.country)
         })
         .attr('font-size', d=>d.population>1000000?11:0)
-        .attr('text-anchor', middle)
+        .attr('fill', 'darkslategray')
+        .attr('text-anchor', 'start')
+})
+
+buildings_data = d3.csv('./data/buildings.csv', d3.autoType).then(buildings_data=>{
+    sorted_buildings = buildings_data.sort((a,b)=>(b.height_m-a.height_m));
+    console.log(sorted_buildings)
+
+    const width = 1500;
+    const height = 500;
+    const svg = d3.select('.buildings-plot').append('svg').attr('width', width).attr('height', height)
+    svg.selectAll('rect')
+        .data(sorted_buildings)
+        .enter()
+        .append('rect')
+        .attr('width', d=>d.height_px)
+        .attr('height', 30)
+        .attr('x', 250)
+        .attr('y', (d,i)=>10+(i*40))
+        .attr('fill', 'orange')
+    svg.selectAll('text.name')
+        .data(sorted_buildings)
+        .enter()
+        .append('text')
+        .text(d=>d.building)
+        .attr('x', 0)
+        .attr('y', (d,i)=>30+(i*40))
+        .attr('font-size', 15)
+        .attr('text-anchor', 'start')
+        .attr('fill', 'darkslategray')
+    svg.selectAll('text.height')
+        .data(sorted_buildings)
+        .enter()
+        .append('text')
+        .text(d=>{
+            return (d.height_ft + ' ft')
+        })
+        // .attr('dx', 200)
+        .attr('x', d=>240+d.height_px)
+        .attr('y', 0)
+        .attr('dy', (d,i)=>30+(i*40))
+        .attr('font-size', 15)
+        .attr('fill', 'white')
+        .attr('text-anchor', 'end')
 })
